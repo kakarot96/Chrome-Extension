bd26cdb4aa9d2d12b42c3ce3226b62d4bd2562fa
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-03-06 15:06:28 +0530

    Changed the domain to hotstar.com

--- Clear&Reload/background.js
+++ Clear&Reload/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.google.com'},
+          pageUrl: {hostEquals: 'www.hotstar.com'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
